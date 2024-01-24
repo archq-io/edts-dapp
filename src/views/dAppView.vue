3561e9cfd1a6805e3e92b844880cc478aa3483d6
Luka Kovacic <luka.kovacic@archq.io>
2024-01-24 17:28:31 +0100

    views: dAppView.vue: Add link to about page and URL input
    Add a link to the about page and an URL input for loading files directly
    from web servers (GitHub, GitLab, static files, ...).
    
    Signed-off-by: Luka Kovacic <luka.kovacic@archq.io>

--- src/views/dAppView.vue
+++ src/views/dAppView.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { ref, reactive, watch } from 'vue'
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter, RouterLink } from 'vue-router';
 import { sha256_digest } from '@/utils/file.js'
 import DragAndDrop from '@/components/DragAndDrop.vue'
 import EthereumPublishDigest from '@/components/EthereumPublishDigest.vue'
 import Badge from '@/components/Badge.vue'
 
 const route = useRoute();
+const router = useRouter();
 
+const enableAbout = ref(import.meta.env.VITE_ENABLE_ABOUT == 'true')
+
+const urlInput = ref('')
 const embedBadge = ref(false)
 let state = reactive({
   file: null,
@@ -23,14 +27,24 @@ function processFileUpload(files) {
   sha256_digest(state.file).then(digest => state.digest = digest)
 }
 
+function urlRouteLocation(url) {
+  return {
+    name: 'dAppView',
+    query: {
+      url: url,
+    },
+  }
+}
 
-watch(() => route.params.digest, digest => {
-  console.log(digest)
-}, { immediate: true })
+function loadFromUrl(url) {
+  if (url == route.query.url) {
+    getFile(url)
+  } else {
+    router.push(urlRouteLocation(url))
+  }
+}
 
-watch(() => route.query.url, url => {
-  if (url) {
-    state.urlPresent = true
+function getFile(url) {
     fetch(url).then(response => {
       response.arrayBuffer().then(value => {
         let contentType = response.headers.get('Content-Type')
@@ -39,6 +53,17 @@ watch(() => route.query.url, url => {
         state.file = file
       })
     })
+}
+
+watch(() => route.params.digest, digest => {
+  console.log(digest)
+}, { immediate: true })
+
+watch(() => route.query.url, url => {
+  if (url) {
+    state.urlPresent = true
+    urlInput.value = url
+    getFile(url)
   } else {
     state.urlPresent = false
   }
@@ -48,7 +73,16 @@ watch(() => route.query.url, url => {
 <template>
   <main>
     <div class="flex flex-col justify-center items-center mt-6">
-      <DragAndDrop v-if="!state.file" @file-upload="processFileUpload" class="m-2" />
+      <div v-if="!state.file" class="flex flex-col justify-center items-center mb-4">
+        <div class="flex flex-col md:flex-row justify-center items-center w-full">
+          <input v-model="urlInput" placeholder="Use an URL (https://raw.githubusercontent.com/...)" class="block h-10 px-3 py-2 w-full md:w-auto md:grow bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500" />
+          <button v-if="urlInput.length > 0" @click="loadFromUrl(urlInput)" class="inline-flex items-center md:grow-0 border rounded-md h-10 md:ml-1 mt-1 md:mt-0 py-1 px-2 hover:bg-gray-200 shadow-sm">
+            <span>Load file</span>
+          </button>
+        </div>
+        <span>or</span>
+        <DragAndDrop @file-upload="processFileUpload" class="mt-2" />
+      </div>
       <EthereumPublishDigest v-if="state.file" :digest="state.digest" />
       <Badge v-if="embedBadge"/>
       <div class="flex flex-row justify-center items-center mt-2">
@@ -61,6 +95,10 @@ watch(() => route.query.url, url => {
           <span v-else>Hide</span>
         </button>
       </div>
+      <RouterLink v-if="enableAbout && !state.file" to="/about" class="inline-flex items-center mx-2 border rounded-md py-1 px-2 hover:bg-gray-200 text-lg leading-tight md:leading-normal">
+        <font-awesome-icon class="mr-2" icon="fa-solid fa-circle-info" />
+        <span>Learn more about decentralized timestamping</span>
+      </RouterLink>
     </div>
   </main>
 </template>
